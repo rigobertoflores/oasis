54d98edd05c880527f679f31c8bbc8c2a3918d3a
Rigo <Rigo@LAPTOP-UB7952GK>
2022-03-07 01:27:22 -0800

    Project test save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+
+import { map } from "rxjs/operators";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'oasis';
+export class AppComponent implements OnInit{
+
+  card: any;
+ NPIDataUrl: string;
+restaurantes:any;
+bares: any;
+private baseUrl = environment;
+
+constructor(public http:HttpClient){
+  this.NPIDataUrl = "";
+}
+  
+  ngOnInit(): void {
+  this.getAllRestaurantes();
+  this.getAllBares();
+  }
+  getAllRestaurantes(){
+    this.getData("https://localhost:44337/weatherforecast/getOsais?categoria=2")
+    .subscribe((res: any) => {
+        this.restaurantes = res;
+        this.card=res[0];
+        console.log(this.restaurantes);
+    });
+  }
+
+  getAllBares(){
+    this.getData("https://localhost:44337/weatherforecast/getOsais?categoria=3")
+    .subscribe((res: any) => {
+        this.bares = res;
+        console.log(this.bares);
+    });
+  }
+  getData(url: string): Observable<any> {
+        return this.http.get(url).pipe(
+            map((response: any) => {
+                if (response) {
+                    if (response.hasError && response.errorCode == 401) {
+                         return false;
+                    }
+                    return response;
+                } else {
+                    return [];
+                }
+            })
+        );
+    }
+    setView(e :any){
+      this.card=e;
+  console.log(e);
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
